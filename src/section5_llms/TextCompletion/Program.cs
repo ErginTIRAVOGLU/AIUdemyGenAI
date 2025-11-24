f2cfdf26829535e3b409036664f7fd56b895d1f5
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2025-11-24 19:24:50 +0300

    Add initial project files and configuration for TextCompletion application
    - Created .gitignore to exclude unnecessary files for Visual Studio and .NET projects.
    - Added solution file for the section5_llms project.
    - Implemented Program.cs to interact with OpenAI API and retrieve responses.
    - Defined TextCompletion.csproj with necessary package references and settings.

---
+++ src/section5_llms/TextCompletion/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+using System.ClientModel;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var credential = new ApiKeyCredential(configuration["GitHubModels:Token"] ?? throw new InvalidOperationException("GitHub Models token not found in configuration."));
+var options = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://models.github.ai/inference")
+};
+
+IChatClient client = new OpenAIClient(credential, options)
+    .GetChatClient("openai/gpt-5-mini").AsIChatClient();
+
+string prompt = "What is AI ? explain max 20 words.";
+Console.WriteLine($"user >>> {prompt}");
+
+ChatResponse response = await client.GetResponseAsync(prompt);
+
+Console.WriteLine($"assistant >>> {response}");
+Console.WriteLine($"Tokens used: in={response.Usage.InputTokenCount}, out={response.Usage.OutputTokenCount}, total={response.Usage.TotalTokenCount}");
